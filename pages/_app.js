373146c71de1a809e77956c0095a3de96690a685
Philipp Bosch <hello@pb.io>
2020-10-26 11:44:34 +0100

    feat: add fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,27 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('WRFQBVSL')
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
